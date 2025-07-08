85b6a33878cb93c94e8bf1f803337e159fa484a3
Joao Pacheco <43354361+jcpach@users.noreply.github.com>
2025-07-08 18:18:46 +0200

    Home and About Me done.

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Messages in a Bottle",
+	url: "https://jcpach.io/",
+	language: "en",
+	description: "Freelance developer who builds software, products, and people. Combining technical expertise with storytelling to create meaningful solutions.",
+	author: {
+		name: "João Pacheco",
+		email: "joao@jcpach.io",
+		url: "https://jcpach.io/about"
+	}
+}
